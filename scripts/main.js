b4d4bd1eaf7585bf883891f9a50e71d56455b9ab
Pavlo Hadzheha <i.w.gadzhega@gmail.com>
2021-08-14 04:48:17 +0300

    added clouds and animated them more or less =-)

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,11 @@
 
 $(function() {
     'use strict';
-    const SCROLL_TIME = 2500;
-    const SCROLL_EASING = 'easeOutQuint';
+    const SCROLL_TIME = 2000;
+    const SCROLL_EASING = 'easeOutCubic';
+
+    const CLOUD_OFFSET = getCloudOffsets();
+    window.CLOUD_OFFSET = getCloudOffsets();
 
     $('.arrow').click(function() {
         let section = $(this).closest('.section');
@@ -17,21 +20,60 @@ $(function() {
 
     $('.nav-dots .dot').click(function() {
         let id = $(this).attr('href');
-        colorNavDot(id);
         let offsetTop = $(id).offset().top;
         animateScroll(offsetTop);
     });
 
     $(window).scroll(() => {
+
+        let within = function(s, plusHalf=true) {
+            let windowCenter = window.scrollY + plusHalf*document.documentElement.clientHeight / 2;
+            return s.top < windowCenter && s.bottom > windowCenter;
+        }
+
+        // Nav-dots change
         let sectionBoundaries = getSectionBoundaries();
-        let windowCenter = window.scrollY + document.documentElement.clientHeight / 2;
         for(let key of Object.keys(sectionBoundaries)){
-            if(windowCenter >= sectionBoundaries[key].top 
-                && windowCenter < sectionBoundaries[key].bottom){ 
+            if(within(sectionBoundaries[key])){ 
                 colorNavDot(key)
             }
         }
+
+
+        // animate clounds;
+        let s1 = sectionBoundaries['#first-flights'];
+        let s2 = sectionBoundaries['#parachute'];
+        let s3 = sectionBoundaries['#english-channel'];
+
+        if(within(s1, false)){
+            const SCROLL_COEFF = [0.1, 0.30, 0.44, 0.1, 0.5]
+            let disp = window.scrollY - s1.top;
+            $('.cloud').filter('.s1').each(function(i,elem){
+                let offset = CLOUD_OFFSET[i];
+                $(elem).css('top', offset + SCROLL_COEFF[i] * disp);
+            });
+        }
+
+        if(within(s2, false)){
+            const SCROLL_COEFF = [0.1, 0.5]
+            let disp = window.scrollY;
+            $('.cloud').filter('.s2').each(function(i,elem){
+                let offset = CLOUD_OFFSET.slice(3,5)[i];
+                $(elem).css('top', offset + SCROLL_COEFF[i] * disp);
+            });
+        }
+
+        if(within(s3, false)){
+            const SCROLL_COEFF = [0.5, 0.5];
+            let disp = window.scrollY - s3.top;
+            $('.cloud').filter('.s3').each(function(i,elem){
+                let offset = CLOUD_OFFSET.slice(-2)[i];
+                $(elem).css('top', offset + SCROLL_COEFF[i] * disp);
+            });
+        }
+
     });
+
 
     function getSectionBoundaries(){
         let obj = {};
@@ -57,6 +99,15 @@ $(function() {
         $('.dot').removeClass('picked');
         $('.dot').filter(`[href*="${ href }"]`).addClass('picked');
     }
+
+    function getCloudOffsets(){
+        let offsets = [];
+        for(const c of $('.cloud')){
+            offsets.push($(c).offset().top);
+        }
+        return offsets;
+    }
+
 
     window.onbeforeunload = function () {
         this.scrollTo(0,0);
